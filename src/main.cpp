e620aa7309b1eab9d83dd48d8858ccf65a6d47a4
tutuskt <arthur.barcellos65@gmail.com>
2022-07-18 17:56:50 -0300

    Criando json para publicação e melhorando sintaxe

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <SPI.h> //biblioteca para comunicação do barramento SPI
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <UUID.h>
 
 #define SS_PIN    21
 #define RST_PIN   22
@@ -30,39 +32,21 @@ WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient); 
 char *mqttServer ="k3s.cos.ufrj.br";
 int mqttPort = 30150;
+const char *topic = "EQUIPAMENTOS";
 
-void setup() {
-  // Inicia a serial
-  Serial.begin(9600);
-  SPI.begin(); // Init SPI bus
-
-  pinMode(pinVerde, OUTPUT);
-  pinMode(pinVermelho, OUTPUT);
-  
-  // Inicia MFRC522
-  mfrc522.PCD_Init(); 
-
-  //setup mqtt broker
-  mqttClient.setServer(mqttServer, mqttPort);
-}
-
-unsigned long getID(){
-  if ( ! mfrc522.PICC_ReadCardSerial()) { //Since a PICC placed get Serial and continue
-    return -1;
-  }
-  unsigned long hex_num;
-  hex_num =  mfrc522.uid.uidByte[0] << 24;
-  hex_num += mfrc522.uid.uidByte[1] << 16;
-  hex_num += mfrc522.uid.uidByte[2] <<  8;
-  hex_num += mfrc522.uid.uidByte[3];
-  mfrc522.PICC_HaltA(); // Stop reading
-  return hex_num;
+String MacAddress;
+/*
+ *  Logging wrapper
+ */
+void log(const char *msg, const char *end = "\n")
+{
+  Serial.print(msg);
+  Serial.print(end);
 }
 
 //faz a leitura dos dados do cartão/tag
 unsigned long leituraDados()
 {
-  //imprime os detalhes tecnicos do cartão/tag
   unsigned long hex_num = -1; //Since a PICC placed get Serial and continue  
   hex_num =  mfrc522.uid.uidByte[0] << 24;
   hex_num += mfrc522.uid.uidByte[1] << 16;
@@ -81,33 +65,117 @@ unsigned long leituraDados()
 //Conecta o esp32 ao wifi
 void connect_wifi(){
 
-  Serial.print("Connecting to ");
-  Serial.println(SSID);  
+  log("[INFO] Conectando na Rede:", SSID);
+  Serial.println("");
   while (WiFi.status() != WL_CONNECTED) {
     WiFi.begin(SSID, PWD);
     digitalWrite(pinVermelho, HIGH);
     delay(5000); //espera conectar para o status mudar
   }
-  Serial.println("Connected.");
-  digitalWrite(pinVermelho, LOW);
-  digitalWrite(pinVerde, HIGH);
-  delay(5000);
-  digitalWrite(pinVerde, LOW);
+  if(WiFi.status() == WL_CONNECTED){
+    MacAddress = WiFi.macAddress();
+    log("[INFO] WiFi connected!");
+    Serial.println("");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.print("MAC: ");
+    Serial.println(MacAddress);
+
+    digitalWrite(pinVermelho, LOW);
+    digitalWrite(pinVerde, HIGH);
+    delay(5000);
+    digitalWrite(pinVerde, LOW);
+  }
+  else{
+    log("[Erro] Falha de Conexão!");
+  }
 }
 
-//Conexão com o broker
-void reconnect() {
-  Serial.println("Connecting to MQTT Broker...");
+//Conexão com o broker MQTT
+void connectMqtt() {  
   while (!mqttClient.connected()) {
-      Serial.println("Reconnecting to MQTT Broker..");
+      Serial.println("");    
+      log("[INFO] Connecting to MQTT Broker...");
       String clientId = "ESP32Client-";
       clientId += String(random(0xffff), HEX);
       
       if (mqttClient.connect(clientId.c_str(),brokerUser, brokerPass)) {
-        Serial.println("Connected.");
+        log("[INFO] Connected.");
+      }
+      else
+      {
+        log("[WARNING] Failed to connect to the MQTT broker, rc=", "");
+        log(mqttClient.state());
+        delay(500);
       }
       
   }
+}
+
+void mqttPublish(unsigned long uid){
+  
+  StaticJsonDocument<200> doc;
+  char json_buffer[200];
+  struct tm timeinfo;
+  String Vazio = "VAZIO";
+  char dataHorabuffer[80];
+  String dataHora;
+  UUID idemPotencyKey;
+  
+  log("[INFO] PUBLICANDO NO BROKER");   
+
+  if (!mqttClient.connected())
+  {
+    connectMqtt();
+  }
+
+  if (!getLocalTime(&timeinfo)){
+    log("[INFO] Data e Hora não foi coletada");
+      dataHora = Vazio.c_str(); 
+      Serial.println(dataHora);
+    
+  }
+  else {
+        strftime(dataHorabuffer, sizeof(dataHorabuffer), "%d/%m/%Y %H:%M:%S", &timeinfo);
+        dataHora = String(dataHorabuffer);
+        Serial.println(dataHora);
+      }
+
+  idemPotencyKey.generate(); //TODO: Não está gerando um novo a cada iteração
+  //UuidCreate(&idemPotencyKey);
+  doc.clear();
+  doc["macAddress"] = MacAddress;
+  doc["dataHoraEvento"] = dataHora;
+  doc["equi_RFID"] = uid;
+  doc["idemPotencyKey"] = idemPotencyKey;
+  
+
+  serializeJsonPretty(doc, json_buffer);
+
+  if (!mqttClient.publish(topic, json_buffer, true))
+  {
+    log("[FALHA] BROKER INACESSIVEL!");
+    delay(500);
+  }
+  else {
+    log("[INFO] Mensagem publicada!");
+    mqttClient.loop();
+  }
+}
+
+void setup() {
+  // Inicia a serial
+  Serial.begin(9600);
+  SPI.begin(); // Init SPI bus
+
+  pinMode(pinVerde, OUTPUT);
+  pinMode(pinVermelho, OUTPUT);
+  
+  // Inicia MFRC522
+  mfrc522.PCD_Init(); 
+
+  //setup mqtt broker
+  mqttClient.setServer(mqttServer, mqttPort);
 }
 
 void loop() 
@@ -117,7 +185,7 @@ void loop()
   }
   
   if (!mqttClient.connected())
-    reconnect();
+    connectMqtt();
   mqttClient.loop();
 
   //Aguarda a aproximacao do cartao
@@ -135,11 +203,11 @@ void loop()
   Serial.println("\nAproxime o seu cartao do leitor...\n");
 
   unsigned long uid = leituraDados();
-  const char *topic = "EQUIPAMENTOS";
-  String macAddress = WiFi.macAddress();
-  String env = "MacAddress:";
-  env += macAddress;
-  mqttClient.publish(topic, env.c_str());
+  mqttPublish(uid);
+  // String macAddress = WiFi.macAddress();
+  // String env = "MacAddress:";
+  // env += macAddress;
+  // mqttClient.publish(topic, env.c_str());
 
   // instrui o PICC quando no estado ACTIVE a ir para um estado de "parada"
   mfrc522.PICC_HaltA(); 
